b5dd488f8d0f76ecc50e8a19614e09bbbed01c5a
Irodoot <Irodoot@gmail.com>
2025-09-19 10:40:43 +0200

    Added RPN + Math

---
+++ RPN + Math/CalculatorController.cs
@@ -0,0 +1,31 @@
+﻿using Calculator.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.Model
+{
+    internal class CalculatorController
+    {
+        RPNCalc rpn = new RPNCalc();
+        CalcMath math = new CalcMath();
+
+
+        public void Run()
+        {
+            string[] input = new string[2];
+            input[0] = "3 4,8 5,7 + * 2,5 *";
+            input[1] = "239 100 4 2 / 5 6 + * - %";
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                rpn.pushStack(input[i]);
+                Console.WriteLine($"Result = {math.Calculate(rpn)}");
+            }
+
+
+        }
+    }
+}
