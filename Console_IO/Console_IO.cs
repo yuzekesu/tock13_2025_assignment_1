a8280cdd75f6649309721172bb92d699a7c74e29
TheVerySmart <yuzekesu@gmail.com>
2025-09-23 19:50:57 +0200

    * added "IO" class. * moved some useful methid in "Console_IO" to "IO" class. * now the "Console_IO" class is inheriting "IO" class.

--- Console_IO/Console_IO.cs
+++ Console_IO/Console_IO.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace Calculator.View
 {
-    public class Console_IO
+    public class Console_IO : IO
     {
         public enum Status
         {
@@ -59,89 +59,6 @@ namespace Calculator.View
         {
             Console.WriteLine($"Result: {result_in_double:f2}");
         }
-        // private methods:
-        private static bool VerifyInput_IsEmpty(string input)
-        {
-            bool isEmpty = true;
-            foreach (char c in input)
-            {
-                if (c != ' ' && c != '\n') // as long there is one valid input.
-                {
-                    isEmpty = false;
-                }
-            }
-            return isEmpty;
-        }
-        private static bool VerifyInput_HasAlphabets(string input, out string theInvalid)
-        {
-            theInvalid = "\0";
-            bool hasAlphabets = false;
-            double temp_number;
-            foreach (string s in input.Split(' ', StringSplitOptions.RemoveEmptyEntries))
-            {
-                if (double.TryParse(s, out temp_number) || VerifyString_IsOperand(s))
-                {
-                    ;
-                }
-                else
-                {
-                    theInvalid = s;
-                    hasAlphabets = true;
-                    break;
-                }
-            }
-            return hasAlphabets;
-        }
-        private static bool VerifyString_IsOperand(string operand)
-        {
-            bool isValid = false;
-            if (operand.Length == 1)
-            {
-                switch (operand[0])
-                {
-                    case '+': isValid = true; break;
-                    case '-': isValid = true; break;
-                    case '*': isValid = true; break;
-                    case '/': isValid = true; break;
-                    case '%': isValid = true; break;
-                }
-            } 
-            
-            return isValid;
-        }
-        private static string EditInput_DiscardsRedundantSpaces(string row_input)
-        {
-            string processed_input = "";
-            foreach (var current in row_input.Split(' ', StringSplitOptions.RemoveEmptyEntries).Select((word, offset) => new {offset, word})) // this is stupid.
-            {
-                if (current.offset == 0)
-                {
-                    processed_input += current.word;
-                }
-                else
-                {
-                    processed_input += ' ';
-                    processed_input += current.word;
-                }
-            }
-            return processed_input;
-        }
-        private static string EditInput_DiscardsTabs(string row_input)
-        {
-            string processed_input = "";
-            foreach (char c in row_input)
-            {
-                if (c != '\t')
-                {
-                    processed_input += c;
-                }
-            }
-            return processed_input;
-        }
-        private static string EditInput_FullCourse(string row_input) 
-        {
-            // get rides of tabs and spaces
-            return EditInput_DiscardsRedundantSpaces(EditInput_DiscardsTabs(row_input));
-        }
+
     }
 }
