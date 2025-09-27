b7e1615a2e7454f7ad5b17ab19a4df139aa13734
TheVerySmart <yuzekesu@gmail.com>
2025-09-27 12:01:44 +0200

    * renamed class for the entry point to "Calculator".

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -4,10 +4,11 @@ using Calculator.Model;
 // To build cross multiple projects:
 // right-click the "Dependecies" in the solution explorer according to your project. Then choose "add references". 
 
-namespace Calculator
+namespace Calculator_Program
 {
-    internal class Program
+    internal class Calculator
     {
+        private CalculatorController controller = new();
         static void Main(string[] args)
         {
             CalculatorController controller = new CalculatorController();
